1435642d195268068d32f73b5e2a409355d4e6a2
Arina <arina.milyutina.0005@gmail.com>
2023-07-08 19:21:00 +0300

    create class Parameters, add fields and annotation

---
+++ src/main/java/com/example/foodtracker/entity/Parameters.java
@@ -0,0 +1,28 @@
+package com.example.foodtracker.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.*;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Parameters {
+    private int age;
+    private int height;
+    private int weight;
+    private String activityLevel;
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    @Id
+    public Long getId() {
+        return id;
+    }
+}
