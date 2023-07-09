96cd7de638e4e26458f992380f602e8fe86c93e6
Arina <arina.milyutina.0005@gmail.com>
2023-07-09 14:39:59 +0300

    add optional

--- src/main/java/com/example/foodtracker/dao/user/UserDao.java
+++ src/main/java/com/example/foodtracker/dao/user/UserDao.java
@@ -3,6 +3,8 @@ package com.example.foodtracker.dao.user;
 import com.example.foodtracker.dao.Dao;
 import com.example.foodtracker.entity.User;
 
+import java.util.Optional;
+
 public interface UserDao extends Dao<User> {
-    User findByUserName(String username);
+    Optional<User> findByUsername (String username);
 }
