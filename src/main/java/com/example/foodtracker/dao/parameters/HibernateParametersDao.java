42627ac2a23700941acfd52c301aa22d99f2f80b
Arina <arina.milyutina.0005@gmail.com>
2023-07-09 23:46:10 +0300

    create class, add method save

---
+++ src/main/java/com/example/foodtracker/dao/parameters/HibernateParametersDao.java
@@ -0,0 +1,19 @@
+package com.example.foodtracker.dao.parameters;
+
+import com.example.foodtracker.dao.Dao;
+import com.example.foodtracker.entity.Parameters;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+public class HibernateParametersDao implements Dao<Parameters> {
+    @PersistenceContext
+    private EntityManager entityManager;
+    @Override
+    public void save(Parameters parameters) {
+        entityManager.persist(parameters);
+    }
+}
