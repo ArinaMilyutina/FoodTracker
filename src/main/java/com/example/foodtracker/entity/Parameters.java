0a1f6b7c8d0378c008003e53849774eb9d4c221b
Arina <arina.milyutina.0005@gmail.com>
2023-07-10 03:07:59 +0300

    add table name

--- src/main/java/com/example/foodtracker/entity/Parameters.java
+++ src/main/java/com/example/foodtracker/entity/Parameters.java
@@ -1,17 +1,20 @@
 package com.example.foodtracker.entity;
 
 import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
 @Getter
 @Setter
+@Table(name = "parameters")
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
-public class Parameters extends AbstractEntity{
+public class Parameters extends AbstractEntity {
     private int age;
     private int height;
     private int weight;
     private double norma;
+    private String activityLevel;
 }
