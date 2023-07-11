7462f867ec4af0a1d24e81b4b3b58d4d3b02c80c
Arina <arina.milyutina.0005@gmail.com>
2023-07-11 22:17:52 +0300

    add checking for the existence of a user when reg and login

--- src/main/java/com/example/foodtracker/controller/UserController.java
+++ src/main/java/com/example/foodtracker/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 
 @Controller
@@ -29,7 +30,8 @@ public class UserController {
     private static final String NEW_USER = "newUser";
     private static final String MESSAGE = "message";
     private static final String USER_NOT_FOUND = "User not found !!!";
-    private static final String INCORRECT_PASSWORD = "Incorrect password !!!";
+    private static final String USER_ALREADY_EXISTS = "User already exists !!!";
+    private static final String INCORRECT_PASSWORD = "Incorrect password or email!!!";
     private static final String NORMA_CALORIES = "Your daily calorie intake for weight maintenance is ";
     private static final String NORMA_CALORIES_WEIGHT_LOSS = ", for weight loss is ";
     private static final String NORMA_CALORIES_WEIGHT_GAIN = ", for weight gain is ";
@@ -38,49 +40,69 @@ public class UserController {
     @Autowired
     private ParametersService parametersService;
 
-    @RequestMapping(value = "/reg",method = RequestMethod.GET)
+    @RequestMapping(value = "/reg", method = RequestMethod.GET)
     public String reg(Model model) {
         model.addAttribute(NEW_USER, new RegistrationDto());
         return "reg";
     }
 
-    @RequestMapping(value = "/reg",method = RequestMethod.POST)
-    public String reg(@ModelAttribute(NEW_USER) @Valid RegistrationDto registrationDto, BindingResult bindingResult) {
+    @RequestMapping(value = "/reg", method = RequestMethod.POST)
+    public String reg(@ModelAttribute(NEW_USER) @Valid RegistrationDto registrationDto, BindingResult bindingResult, Model model) {
         if (bindingResult.hasErrors()) {
             return "reg";
         }
-        User user = User.builder()
-                .name(registrationDto.getName())
-                .username(registrationDto.getUsername())
-                .password(registrationDto.getPassword())
-                .email(registrationDto.getEmail())
-                .build();
-        userService.createUser(user);
-        return "redirect:/auth";
+        String findUser = userService.findUser(registrationDto.getUsername());
+        if (findUser.equals("false")) {
+            User user = User.builder()
+                    .name(registrationDto.getName())
+                    .username(registrationDto.getUsername())
+                    .password(registrationDto.getPassword())
+                    .email(registrationDto.getEmail())
+                    .build();
+            userService.createUser(user);
+            return "redirect:/auth";
 
+        }
+        model.addAttribute(MESSAGE, USER_ALREADY_EXISTS);
+        return "reg";
     }
 
-    @RequestMapping(value = "/auth",method = RequestMethod.GET)
+    @RequestMapping(value = "/auth", method = RequestMethod.GET)
     public String auth(Model model) {
+
         model.addAttribute(LOGIN_USER, new LoginDto());
         return "auth";
     }
 
-    @RequestMapping(value = "/auth",method = RequestMethod.POST)
+    @RequestMapping(value = "/auth", method = RequestMethod.POST)
     public String auth(@ModelAttribute(LOGIN_USER) LoginDto loginDto, Model model, HttpSession httpSession) {
+        String findUser = userService.findUser(loginDto.getUsername());
+        if (findUser.equals("true")) {
+            Optional<User> findByUsername = userService.findByUsername(loginDto.getUsername());
+            User user = findByUsername.get();
+            if (user.getPassword().equals(loginDto.getPassword())) {
+                httpSession.setAttribute(USER, user);
+                return "redirect:/parameters";
+            } else {
+                model.addAttribute(MESSAGE, INCORRECT_PASSWORD);
 
-        return "redirect:/parameters";
+            }
+        } else {
+            model.addAttribute(MESSAGE, USER_NOT_FOUND);
+        }
+        return "auth";
 
     }
 
-    @RequestMapping(value = "/parameters",method = RequestMethod.GET)
+
+    @RequestMapping(value = "/parameters", method = RequestMethod.GET)
     public String parameters(Model model) {
         model.addAttribute(PARAMETERS, new Parameters());
         return "parameters";
     }
 
-    @RequestMapping(value = "/parameters",method = RequestMethod.POST)
-    public String parameters(@ModelAttribute(PARAMETERS)  Parameters parameters, ParametersDto parametersDto, Model model, BindingResult bindingResult) {
+    @RequestMapping(value = "/parameters", method = RequestMethod.POST)
+    public String parameters(@ModelAttribute(PARAMETERS) Parameters parameters, ParametersDto parametersDto, Model model, BindingResult bindingResult) {
         double normaOfCalories = parametersService.calculateNormaOfCalories(parameters);
         double normaOfCaloriesForWeightLoss = parametersService.calculateNormaOfCaloriesForWeightLoss(parameters);
         double normaOfCaloriesForWeightGain = parametersService.calculateNormaOfCaloriesForWeightGain(parameters);
