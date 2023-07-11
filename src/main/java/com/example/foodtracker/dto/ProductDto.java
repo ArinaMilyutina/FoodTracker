3d84d61e0df5174c4262351aba04bb937ca3e3eb
Arina <arina.milyutina.0005@gmail.com>
2023-07-12 00:43:33 +0300

    create dto for class Product

---
+++ src/main/java/com/example/foodtracker/dto/ProductDto.java
@@ -0,0 +1,17 @@
+package com.example.foodtracker.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ProductDto {
+    private int calories;
+    private int proteins;
+    private int fats;
+    private int carbohydrates;
+    private String productName;
+    private int barcode;
+}
