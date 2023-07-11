a8bed7e78e202adfee33825ec5c913467dcd37fa
Arina <arina.milyutina.0005@gmail.com>
2023-07-12 01:16:11 +0300

    add method for search products and save

---
+++ src/main/java/com/example/foodtracker/dao/product/HibernateProductDao.java
@@ -0,0 +1,28 @@
+package com.example.foodtracker.dao.product;
+
+
+import com.example.foodtracker.entity.product.Product;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.springframework.stereotype.Service;
+
+
+@Service
+public class HibernateProductDao implements ProductDao {
+    private static final String FIND_PRODUCT_BY_NAME = "from Product where productName = :productName";
+    private static final String PRODUCT_NAME = "productName";
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Override
+    public void save(Product product) {
+        entityManager.persist(product);
+    }
+
+    @Override
+    public Product findProductByName(String productName) {
+        return entityManager.createQuery(FIND_PRODUCT_BY_NAME, Product.class)
+                .setParameter(PRODUCT_NAME, productName)
+                .getSingleResult();
+    }
+}
