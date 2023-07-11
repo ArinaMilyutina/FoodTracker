b12795b71a64920efd81b5364dd9c224d7d40e89
Arina <arina.milyutina.0005@gmail.com>
2023-07-12 00:59:22 +0300

    delete annotation Transactional

--- src/main/java/com/example/foodtracker/dao/parameters/HibernateParametersDao.java
+++ src/main/java/com/example/foodtracker/dao/parameters/HibernateParametersDao.java
@@ -1,14 +1,12 @@
 package com.example.foodtracker.dao.parameters;
 
 import com.example.foodtracker.dao.Dao;
-import com.example.foodtracker.entity.Parameters;
+import com.example.foodtracker.entity.parameters.Parameters;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
-@Transactional
 public class HibernateParametersDao implements Dao<Parameters> {
     @PersistenceContext
     private EntityManager entityManager;
