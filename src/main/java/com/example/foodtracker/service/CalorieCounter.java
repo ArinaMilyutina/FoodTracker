b53c87c1808f08c6b87459a64280ea1a68e13696
Arina <arina.milyutina.0005@gmail.com>
2023-07-11 03:29:13 +0300

    rename class,add method calculateNormaOfCalories

--- src/main/java/com/example/foodtracker/service/CalorieCounter.java
+++ src/main/java/com/example/foodtracker/service/CalorieCounter.java
@@ -1,11 +1,12 @@
 package com.example.foodtracker.service;
 
 
+import com.example.foodtracker.entity.Parameters;
 import org.springframework.stereotype.Component;
 
 
 @Component
-public class NormOfCalories {
+public class CalorieCounter {
     private static final double MINIMUM = 1.2;
     private static final double LOW = 1.375;
     private static final double AVERAGE = 1.55;
@@ -37,6 +38,29 @@ public class NormOfCalories {
 
     private static double normaOfCalories(int age, int height, int weight) {
         return weight * 10 + height * 6.25 - age * 5;
+    }
+
+    public double calculateNormaOfCalories(Parameters parameters) {
+
+        switch (parameters.getActivityLevel()) {
+            case "minimum" -> {
+                return minimumLevel(parameters.getAge(), parameters.getHeight(), parameters.getWeight());
+            }
+            case "low" -> {
+                return lowLevel(parameters.getAge(), parameters.getHeight(), parameters.getWeight());
+            }
+            case "average" -> {
+                return averageLevel(parameters.getAge(), parameters.getHeight(), parameters.getWeight());
+            }
+            case "high" -> {
+                return highLevel(parameters.getAge(), parameters.getHeight(), parameters.getWeight());
+            }
+            case "very high" -> {
+                return veryHighLevel(parameters.getAge(), parameters.getHeight(), parameters.getWeight());
+            }
+        }
+        return 0;
+
     }
 
 }
