8fad5040e68436ff858fc4bba2ad9d6d37114d15
Arina <arina.milyutina.0005@gmail.com>
2023-07-11 03:29:37 +0300

    delete method calculateNormaOfCalories

--- src/main/java/com/example/foodtracker/service/ParametersService.java
+++ src/main/java/com/example/foodtracker/service/ParametersService.java
@@ -7,43 +7,21 @@ import org.springframework.stereotype.Service;
 
 
 @Service
-public class ParametersService extends NormOfCalories {
-    private static final String MESSAGE = "Your norm of calories to maintain weight is equal to ";
+public class ParametersService {
 
     @Autowired
     private HibernateParametersDao hibernateParametersDao;
+    @Autowired
+    private CalorieCounter calorieCounter;
 
 
     public void createParameters(Parameters parameters) {
         hibernateParametersDao.save(parameters);
     }
 
+
     public double calculateNormaOfCalories(Parameters parameters) {
-
-        switch (parameters.getActivityLevel()) {
-            case "minimum" -> {
-
-                return minimumLevel(parameters.getAge(),parameters.getHeight(),parameters.getWeight());
-            }
-            case "low" -> {
-//                parametersDto.setNorma(lowLevel(parametersDto.getAge(), parametersDto.getHeight(), parametersDto.getWeight()));
-//                return parameter;
-            }
-            case "average" -> {
-//                parametersDto.setNorma(averageLevel(parametersDto.getAge(), parametersDto.getHeight(), parametersDto.getWeight()));
-//                return parameter;
-            }
-            case "high" -> {
-//                parametersDto.setNorma(highLevel(parametersDto.getAge(), parametersDto.getHeight(), parametersDto.getWeight()));
-//                return parameter;
-            }
-            case "very high" -> {
-//                parametersDto.setNorma(veryHighLevel(parametersDto.getAge(), parametersDto.getHeight(), parametersDto.getWeight()));
-//                return parameter;
-            }
-        }
-        return 0;
-
+        return calorieCounter.calculateNormaOfCalories(parameters);
     }
 
 }
