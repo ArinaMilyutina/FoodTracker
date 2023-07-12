8441e65f756b4bcec3e63eb62835a6933f7a121a
Arina <arina.milyutina.0005@gmail.com>
2023-07-13 00:50:34 +0300

    changed the type of the returned value in method findProductByName (Product->List<Product>)

--- src/main/java/com/example/foodtracker/dao/product/ProductDao.java
+++ src/main/java/com/example/foodtracker/dao/product/ProductDao.java
@@ -3,8 +3,11 @@ package com.example.foodtracker.dao.product;
 import com.example.foodtracker.dao.Dao;
 import com.example.foodtracker.entity.product.Product;
 
+import java.util.List;
+
 public interface ProductDao extends Dao<Product> {
-    Product findProductByName(String productName);
+    List<Product> findProductByName(String productName);
 
     String findProductByBarcode(String barcode);
+
 }
