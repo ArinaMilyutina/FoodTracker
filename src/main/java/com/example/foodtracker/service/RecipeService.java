98161e6f5449b0272df524f8c6df07f7edd4a530
Arina <arina.milyutina.0005@gmail.com>
2023-07-15 11:43:25 +0300

    create service for recipe, add method save

--- src/main/java/com/example/foodtracker/service/RecipeService.java
+++ src/main/java/com/example/foodtracker/service/RecipeService.java
@@ -1,7 +1,7 @@
 package com.example.foodtracker.service;
 
 import com.example.foodtracker.dao.recipe.HibernateRecipeDao;
-import com.example.foodtracker.entity.Recipe;
+import com.example.foodtracker.entity.recipe.Recipe;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -13,4 +13,5 @@ public class RecipeService {
     public void createRecipe(Recipe recipe) {
         hibernateRecipeDao.save(recipe);
     }
+
 }
