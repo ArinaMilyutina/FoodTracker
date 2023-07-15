0a59552827f9b4d33421d393bbd855639ade939e
Arina <arina.milyutina.0005@gmail.com>
2023-07-15 11:00:11 +0300

    create entity recipe

--- src/main/java/com/example/foodtracker/entity/recipe/Recipe.java
+++ src/main/java/com/example/foodtracker/entity/recipe/Recipe.java
@@ -1,5 +1,7 @@
-package com.example.foodtracker.entity;
+package com.example.foodtracker.entity.recipe;
 
+import com.example.foodtracker.entity.AbstractEntity;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Lob;
 import jakarta.persistence.Table;
@@ -16,5 +18,6 @@ public class Recipe extends AbstractEntity {
     private String name;
     private String description;
     @Lob
+    @Column(name = "image", length = 1000)
     private byte[] image;
 }
