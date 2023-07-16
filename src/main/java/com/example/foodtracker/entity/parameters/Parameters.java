40a96c2233bf1778b6bb1184bf3e5a331249f2e9
Arina <arina.milyutina.0005@gmail.com>
2023-07-17 02:54:56 +0300

    add validation

--- src/main/java/com/example/foodtracker/entity/parameters/Parameters.java
+++ src/main/java/com/example/foodtracker/entity/parameters/Parameters.java
@@ -2,6 +2,7 @@ package com.example.foodtracker.entity.parameters;
 
 import com.example.foodtracker.entity.AbstractEntity;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.DecimalMin;
 import lombok.*;
 
 import java.util.List;
@@ -14,8 +15,11 @@ import java.util.List;
 @NoArgsConstructor
 @Builder
 public class Parameters extends AbstractEntity {
+    @DecimalMin(value = "0")
     private int age;
+    @DecimalMin(value = "0")
     private int height;
+    @DecimalMin(value = "0")
     private int weight;
     @Enumerated(EnumType.STRING)
     private ActivityLevel activityLevel;
