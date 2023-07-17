9ad25389024ffd03e4ec0d4448f1d0dca6db2f62
Arina <arina.milyutina.0005@gmail.com>
2023-07-17 05:12:30 +0300

    add validation

--- src/main/java/com/example/foodtracker/dto/ParametersDto.java
+++ src/main/java/com/example/foodtracker/dto/ParametersDto.java
@@ -1,6 +1,8 @@
 package com.example.foodtracker.dto;
 
-import com.example.foodtracker.entity.parameters.ActivityLevel;
+import com.example.foodtracker.entity.user.ActivityLevel;
+import jakarta.validation.constraints.DecimalMin;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -9,11 +11,18 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 @NoArgsConstructor
 public class ParametersDto {
+    @NotNull
+    @DecimalMin(value = "0", message = "Check the correctness of the entered data.")
     private int age;
+    @NotNull
+    @DecimalMin(value = "0", message = "Check the correctness of the entered data.")
     private int height;
+    @NotNull
+    @DecimalMin(value = "0", message = "Check the correctness of the entered data.")
     private int weight;
     private double normaOfCalories;
     private double normaOfCaloriesForWeightLoss;
     private double normaOfCaloriesForWeightGain;
+    @NotNull
     private ActivityLevel activityLevel;
 }
